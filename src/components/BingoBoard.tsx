b3ad8346b7136fb3232a617511a9b3936054ae00
mohazerro <mohazerr@outlook.fr>
2025-01-22 13:07:05 +0100

    textsize and padding on cards

--- src/components/BingoBoard.tsx
+++ src/components/BingoBoard.tsx
@@ -39,7 +39,7 @@ export async function BingoBoard({ bingo }: { bingo: BingoWithCells }) {
         </h2>
         <SwitchTheme />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
         {cells}
       </div>
     </div>
